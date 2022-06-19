5239bbcfca5592c7af94e24e9346a7772cbb4144
Konstantin Velly <konstantinvelly@gmail.com>
2022-06-19 11:34:27 +0300

    Additional commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
 
     public double annuity(int creditSum, double rate, int months) {
-        double payment = creditSum * ((rate * Math.pow((1 + rate), months)) / (Math.pow((1 + rate), months)-1));
+        double payment = creditSum * ((rate * Math.pow((1 + rate), months)) / (Math.pow((1 + rate), months) - 1));
         //Формула аннуитета: n * ((r * Math.pow((1 + r), 12 * y)) / (Math.pow((1 + r), 12 * y) - 1));
         return payment;
     }
